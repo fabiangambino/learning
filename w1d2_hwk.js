ee61be29b4499a34050996fbe97864baaa075302
Fabian Gambino <34524226+fabiangambino@users.noreply.github.com>
2020-02-26 01:15:42 -0500

    Add files via upload

---
+++ w1d2_hwk.js
@@ -0,0 +1,62 @@
+// isOdd
+
+// Write a function `isOdd` that takes in a number as an argument and returns `true`
+// if the number is odd and returns `false` otherwise.
+// Write two versions of this function, using conditionals and without using conditionals
+//
+// Examples:
+// console.log(isOdd(2));    // => false
+// console.log(isOdd(5));    // => true
+// console.log(isOdd(-17));  // => true
+
+// isOdd with CONDITIONAL
+function isOdd(num) {
+  if (num % 2 !== 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// isOdd without conditional
+function isOdd(num) {
+  return num % 2 !== 0;
+}
+
+//plusFive
+
+// Write a function `plusFive` that takes in a number as an argument and returns
+// the sum of that number and 5.
+//
+// Examples:
+// console.log(plusFive(10));  // => 15
+// console.log(plusFive(2));   // => 7
+// console.log(plusFive(-8));  // => -3
+//
+
+function plusFive(num) {
+  return num + 5;
+}
+
+//threeOrSeven
+
+// Write a function `threeOrSeven` that takes in a number and returns `true`
+// if the number is divisible by either 3 or 7 and `false` otherwise.
+// Write two versions of this function, using conditionals and without using conditionals
+//
+//
+// Examples:
+// threeOrSeven(3);   // => true
+// threeOrSeven(42);  // => true
+// threeOrSeven(8);   // => false
+//
+
+// theeOrSeven with CONDITIONAL
+function threeOrSeven(num) {
+  
+}
+
+// theeOrSeven without condtional
+function threeOrSeven(num) {
+
+}
